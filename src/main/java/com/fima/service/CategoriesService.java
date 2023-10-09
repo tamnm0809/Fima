cf19191e3fb75c233d33034d31a46818c77e1777
NgyenMinhTam <tamnm0809@gmail.com>
2023-10-09 16:48:36 +0700

    commit

--- src/main/java/com/fima/service/CategoriesService.java
+++ src/main/java/com/fima/service/CategoriesService.java
@@ -1,8 +1,5 @@
 package com.fima.service;
 
-import java.util.List;
-
 public interface CategoriesService{
 	
-	public List<String> getAllCategoriesByName();
 }
--- src/main/java/com/fima/service/impl/CategoriesServiceImpl.java
+++ src/main/java/com/fima/service/impl/CategoriesServiceImpl.java
@@ -1,38 +1,14 @@
 package com.fima.service.impl;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
 import com.fima.repository.CategoriesRepository;
 import com.fima.service.CategoriesService;
 
 @Service
-public class CategoriesServiceImpl implements CategoriesService{
-	
-	@Autowired
-	CategoriesRepository categoriesRepository;
-	
-	public List<String> getAllCategoriesByName(){
-		return categoriesRepository.getCategoriesByName();
-=======
-import com.fima.entity.Categories;
-import com.fima.repository.CategoriesRepository;
+public class CategoriesServiceImpl implements CategoriesService {
 
-@Service
-public class CategoriesServiceImpl {
 	@Autowired
 	CategoriesRepository categoriesRepository;
-	
-	
-	public List<Categories> getAllCategories() {
-		return categoriesRepository.findAll();
-	}
-	
-	public void deleteCategories(Long id) {
-		categoriesRepository.deleteById(id);
->>>>>>> af5eb660ac543a4043639597f5357470a68d03a4
-	}
 }
